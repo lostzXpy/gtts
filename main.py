61f70075811c6675a4378dd90d10278c0043e10d
LostzX <79033314+lostzXpy@users.noreply.github.com>
2021-05-27 19:01:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from os import system
+from time import sleep
+
+# Cores
+
+f_vermelho = '\033[1;41m'
+f_verde = '\033[1;42m'
+f_amarelo = '\033[1;43m'
+
+l_amarelo = '\033[1;33m'
+l_vermelho = '\033[1;31m'
+l_verde = '\033[1;32m'
+
+no_color = '\033[m'
+
+# Instalação
 
 def install():
+    print(f'\n\n[{l_vermelho}i{no_color}] {l_vermelho}Instalando recursos...\n\n')
     system('pkg install mpg123')
-
-def clear():
-    system('clear')
-
-def main():
     
+def gtts():
+    system('clear')
+    
+    tts = gTTS(text=texto, lang=idioma, slow=False)
+    tts.save("gtts.mp3")
+    system("mpg123 gtts.mp3")
