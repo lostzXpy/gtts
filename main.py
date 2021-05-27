c996645a2520de22dc2f167f2ab60b0e72072575
LostzX <79033314+lostzXpy@users.noreply.github.com>
2021-05-27 19:21:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,16 +14,6 @@ l_verde = '\033[1;32m'
 
 no_color = '\033[m'
 
-# Instalação
-
-def install():
-    check = subprocess.check_output('pkg install mpg123', shell=True)
-    if '0 upgraded, 0 newly installed, 0 to remove and 1 not upgraded' not in check.decode():
-        print(f'\n\n[{l_vermelho}i{no_color}] {l_vermelho}Instalando recursos...\n\n')
-        system('pkg install mpg123')
-    else:
-        print(f'\n\n[{l_vermelho}i{no_color}] {l_vermelho}Iniciando...\n\n')
-        sleep(1)
 # GTTS
 
 def gtts():
@@ -36,5 +26,5 @@ def gtts():
     print(f'\n{f_vermelho}Resultado:{no_color}\n')
     system("mpg123 gtts.mp3")
 
-install()
+
 gtts()
